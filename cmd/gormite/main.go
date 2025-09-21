b15aa4ecee59052fa8df91115925e545776dc363
KoNekoD <moskva111@yahoo.com>
2025-09-21 12:32:27 +0400

    feat: schema validate command

--- cmd/gormite/main.go
+++ cmd/gormite/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"os"
+	"slices"
+
 	"github.com/KoNekoD/gormite/pkg/runners"
 	"github.com/gookit/goutil/cflag"
 	"github.com/pkg/errors"
-	"slices"
 )
 
 func toolValidate(val any) (err error) {
@@ -15,37 +17,30 @@ func toolValidate(val any) (err error) {
 	return err
 }
 
+var possibleScenarios = []string{runners.ScenarioTypeDiff, runners.ScenarioTypeValidate}
+
 func main() {
-	ctx := context.Background()
-	opts := runners.DiffRunnerOptions{}
-
-	c := cflag.New(func(c *cflag.CFlags) { c.Desc = "Create migrations diff" })
-
-	c.StringVar(
-		&opts.Tool,
-		"tool",
-		"",
-		"migration tool, allowed: migrate, goose;true;t,mt,m",
-	)
-	c.StringVar(
-		&opts.Dsn,
-		"dsn",
-		"",
-		"database connection string;true;dsn,db,d",
-	)
-
-	c.StringVar(
-		&opts.ConfigPath,
-		"config-path",
-		"resources/gormite.yaml",
-		"config path;true;config,c",
-	)
-
-	c.AddValidator("tool", toolValidate)
-
-	c.Func = func(c *cflag.CFlags) error {
-		return runners.NewDiffRunner(opts).Run(ctx)
+	scenario := runners.ScenarioTypeDiff
+	args := os.Args[1:]
+	if len(args) > 0 && slices.Contains(possibleScenarios, args[0]) {
+		scenario = args[0]
+		args = args[1:]
 	}
 
-	c.MustParse(nil)
+	c := cflag.New(func(c *cflag.CFlags) { c.Desc = "Gormite CLI" })
+	ctx := context.Background()
+	opts := runners.DiffRunnerOptions{Scenario: scenario}
+
+	switch scenario {
+	case runners.ScenarioTypeDiff:
+		c.StringVar(&opts.Tool, "tool", "", "migration tool, allowed: migrate, goose;true;t")
+		c.AddValidator("tool", toolValidate)
+	}
+
+	c.StringVar(&opts.Dsn, "dsn", "", "database connection string;true")
+	c.StringVar(&opts.ConfigPath, "config", "gormite.yaml", "config file path;true;config,c")
+
+	c.Func = func(c *cflag.CFlags) error { return runners.NewDiffRunner(opts).Run(ctx) }
+
+	c.MustParse(args)
 }
--- pkg/runners/diff_runner.go
+++ pkg/runners/diff_runner.go
@@ -9,6 +9,7 @@ import (
 	"github.com/KoNekoD/gormite/pkg/platforms"
 	"github.com/KoNekoD/gormite/pkg/platforms/postgres_platform"
 	"github.com/KoNekoD/gormite/pkg/schema_managers/postgres_schema_manager"
+	"github.com/charmbracelet/log"
 	"github.com/pkg/errors"
 	"os"
 	"time"
@@ -32,10 +33,16 @@ const (
 	MigrationToolTypeMigrate MigrationToolType = "migrate"
 )
 
+const (
+	ScenarioTypeDiff     = "diff"
+	ScenarioTypeValidate = "validate"
+)
+
 type DiffRunnerOptions struct {
 	Tool       string
 	Dsn        string
 	ConfigPath string
+	Scenario   string
 }
 
 type DiffRunner struct{ opts DiffRunnerOptions }
@@ -45,16 +52,11 @@ func NewDiffRunner(opts DiffRunnerOptions) *DiffRunner {
 }
 
 func (r *DiffRunner) Run(ctx context.Context) error {
-	d := gormite_databases.NewPostgresDatabase(ctx, r.opts.Dsn)
+	db := gormite_databases.NewPostgresDatabase(ctx, r.opts.Dsn)
 
-	p := postgres_platform.NewPostgreSQLPlatform()
+	platform := postgres_platform.NewPostgreSQLPlatform()
 
-	manager := postgres_schema_manager.NewPostgreSQLSchemaManager(
-		platforms.NewConnection(
-			d,
-			postgres_platform.NewPostgreSQLPlatform(),
-		), p,
-	)
+	manager := postgres_schema_manager.NewPostgreSQLSchemaManager(platforms.NewConnection(db, platform), platform)
 
 	oldSchema := manager.IntrospectSchema()
 
@@ -63,39 +65,58 @@ func (r *DiffRunner) Run(ctx context.Context) error {
 		return errors.Wrap(err, "failed to introspect local schema")
 	}
 
-	c := diff_calc.NewComparator(p)
+	c := diff_calc.NewComparator(platform)
 
 	diff := c.CompareSchemas(oldSchema, newSchema)
 	diffDown := c.CompareSchemas(newSchema, oldSchema)
 
-	if diff.IsEmpty() {
-		return errors.New("No changes detected")
-	}
+	switch r.opts.Scenario {
+	case ScenarioTypeDiff:
+		if diff.IsEmpty() {
+			return errors.New("No changes detected")
+		}
 
-	up := manager.AlterSchema(diff)
-	down := manager.AlterSchema(diffDown)
+		up := manager.AlterSchema(diff)
+		down := manager.AlterSchema(diffDown)
 
-	switch r.opts.Tool {
-	case string(MigrationToolTypeMigrate):
-		key := time.Now().Format("20060102150405")
-		file := fmt.Sprintf("migrations/%s_gen.up.sql", key)
-		fileDown := fmt.Sprintf("migrations/%s_gen.down.sql", key)
-		if err := os.WriteFile(file, []byte(up), 0644); err != nil {
-			return errors.Wrap(err, "Cannot write up sql file")
+		switch r.opts.Tool {
+		case string(MigrationToolTypeMigrate):
+			key := time.Now().Format("20060102150405")
+			file := fmt.Sprintf("migrations/%s_gen.up.sql", key)
+			fileDown := fmt.Sprintf("migrations/%s_gen.down.sql", key)
+			if err := os.WriteFile(file, []byte(up), 0644); err != nil {
+				return errors.Wrap(err, "Cannot write up sql file")
+			}
+			if err := os.WriteFile(fileDown, []byte(down), 0644); err != nil {
+				return errors.Wrap(err, "Cannot write down sql file")
+			}
+		case string(MigrationToolTypeGoose):
+			key := time.Now().Format("20060102150405")
+			file := fmt.Sprintf("migrations/%s_gen.sql", key)
+			if err := os.WriteFile(
+				file,
+				[]byte(fmt.Sprintf(GooseMigrationTemplate, up, down)),
+				0644,
+			); err != nil {
+				return errors.Wrap(err, "Cannot write migration file")
+			}
 		}
-		if err := os.WriteFile(fileDown, []byte(down), 0644); err != nil {
-			return errors.Wrap(err, "Cannot write down sql file")
+	case ScenarioTypeValidate:
+		if diff.IsEmpty() {
+			log.Info("The database schema is in sync with the mapping files.")
+			return nil
 		}
-	case string(MigrationToolTypeGoose):
-		key := time.Now().Format("20060102150405")
-		file := fmt.Sprintf("migrations/%s_gen.sql", key)
-		if err := os.WriteFile(
-			file,
-			[]byte(fmt.Sprintf(GooseMigrationTemplate, up, down)),
-			0644,
-		); err != nil {
-			return errors.Wrap(err, "Cannot write migration file")
+
+		sqlList := manager.AlterSchemaSqlList(diff)
+
+		log.Error("The database schema is not in sync with the current mapping file.")
+
+		log.Infof("%d schema diff(s) detected:", len(sqlList))
+		for _, sql := range sqlList {
+			log.Infof("    %s", sql)
 		}
+
+		return errors.New("The database schema is not in sync with the current mapping file.")
 	}
 
 	return nil
